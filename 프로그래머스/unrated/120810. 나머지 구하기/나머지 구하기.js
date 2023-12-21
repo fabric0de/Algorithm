9ac9fcf548baf68ca5376c37dcdc4231b48ab933
fabric0de <97182642+fabric0de@users.noreply.github.com>
2023-12-21 16:26:11 +0900

    [level unrated] Title: 나머지 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120810. 나머지 구하기/나머지 구하기.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+    if ( 0 <= num1 && num1 <= 100 ){
+        if ( 0 <= num2 && num2 <= 100 ){
+            var answer = num1 % num2;
+            return answer;
+        }
+    }
+    
+}

4cdcac049bf2fcc3cfd82d5ff2d7ccc79985f017
fabric0de <97182642+fabric0de@users.noreply.github.com>
2023-12-21 17:25:45 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    var answer = [];
+    var boolean;
+    if ( 1 <= num_list.length && num_list.length <= 1000 ) {
+        for ( i = 0; i < num_list.length;  i++) {
+            if ( 0 <= num_list[i] && num_list[i] <= 1000 ) {
+                boolean = true;
+            }
+        }
+    }
+    if (boolean){
+        answer = num_list.reverse();
+        return answer;
+    }
+}

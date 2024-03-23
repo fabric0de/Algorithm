5fd061b90201292b81c461a6ed8eb30f8acba912
김정현 (Kim JungHyeon) <solee3013@gmail.com>
2024-03-23 23:39:30 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,20 @@
+function solution(numer1, denom1, numer2, denom2) {
+  
+  let numer = numer1 * denom2 + numer2 * denom1;
+  let denom = denom1 * denom2;
+    
+  let divisor = gcd(numer, denom);
+  numer /= divisor;
+  denom /= divisor;
+
+  return [numer, denom];
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}

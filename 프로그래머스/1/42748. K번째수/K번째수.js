cd4d9a77647bed2c03f0e7e4684cb14858fd1eb8
김정현 (Kim JungHyeon) <solee3013@gmail.com>
2024-03-31 00:59:25 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,7 @@
 function solution(array, commands) {
-    var answer = [];
-    for ( let i = 0; i < commands.length; i++ ) {
-        let newArr = array.slice(commands[i][0]-1,commands[i][1]);
-        newArr.sort((a,b) => a-b);
-        console.log(newArr)
-        answer.push(newArr[commands[i][2]-1]);   
-    }
-    return answer;
+    return commands.map(([start, end, k]) => { //map + 구조분해할당
+        const newArr = array.slice(start - 1, end).sort((a, b) => a - b);
+        
+        return newArr[k - 1];
+    });
 }

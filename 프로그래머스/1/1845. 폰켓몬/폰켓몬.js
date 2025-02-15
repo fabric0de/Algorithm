8dd3df475801d4e55fa6a49a219781f9d4102989
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2025-02-15 22:19:34 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,18 +1,5 @@
 function solution(nums) {
-    var answer = 0;
-    
-    const poke = nums.length / 2
-    
-    const set = new Set(nums)
-    const uniqueArr = [...set]
-    const uniqueArrLength = uniqueArr.length
-    
-    if (uniqueArrLength < poke) {
-        answer = uniqueArrLength
-    } else {
-        answer = poke
-    }
-    
-    
-    return answer;
+    const uniqueArrLength = new Set(nums).size;  
+    const poke = nums.length / 2;                
+    return Math.min(uniqueArrLength, poke);      
 }

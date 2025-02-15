467df552bb262026e0d613b29710e35fe1628190
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2025-02-15 22:55:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.97 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,20 +1,17 @@
 function solution(participant, completion) {
-    const map = new Map();
-
-    // 참가자들의 이름과 등장 횟수를 맵에 저장
-    for (const name of participant) {
-        map.set(name, (map.get(name) || 0) + 1);
+    var answer = '';
+    const duplicate = {}
+    const complete = {}
+    participant.forEach((x) => { 
+        duplicate[x] = (duplicate[x] || 0)+1; 
+    });
+    completion.forEach((x) => { 
+        complete[x] = (complete[x] || 0)+1; 
+    });
+     
+    for (let key of participant) {
+        if (duplicate[key] !== complete[key]) {
+        return key;
     }
-
-    // 완주한 선수들의 이름으로 맵에서 해당 이름의 횟수를 감소
-    for (const name of completion) {
-        map.set(name, map.get(name) - 1);
-    }
-
-    // 맵을 순회하면서 값이 0보다 큰 첫 번째 키(참가자)를 반환
-    for (const [name, count] of map) {
-        if (count > 0) {
-            return name;
-        }
-    }
+  }
 }
